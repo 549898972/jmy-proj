c5a38305683a4fb1e04adedf07b3d7db950334f0
jiangmuyuan <jiangmuyuan@sogou-inc.com>
2019-08-15 15:22:38 +0800

    提交

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -2,16 +2,36 @@
     <div class="swipe-container">
         <!-- 轮播图 -->
         <van-swipe :autoplay="3000" indicator-color="white">
-            <van-swipe-item>1</van-swipe-item>
-            <van-swipe-item>2</van-swipe-item>
-            <van-swipe-item>3</van-swipe-item>
-            <van-swipe-item>4</van-swipe-item>
+            <van-swipe-item><img src="../../images/timg.jpg" style="width: 100%;height: 100%"></van-swipe-item>
+            <van-swipe-item><img src="../../images/timg2.jpg" style="width: 100%;height: 100%"></van-swipe-item>
         </van-swipe>
+        <van-grid square column-num="3" class="grid-container">
+            <van-grid-item ><div class="grid-item"><img src="../../images/menu1.png" alt=""><p>文字</p></div></van-grid-item>
+            <van-grid-item ><div class="grid-item"><img src="../../images/menu2.png" alt=""><p>文字</p></div></van-grid-item>
+            <van-grid-item ><div class="grid-item"><img src="../../images/menu3.png" alt=""><p>文字</p></div></van-grid-item>
+            <van-grid-item ><div class="grid-item"><img src="../../images/menu4.png" alt=""><p>文字</p></div></van-grid-item>
+            <van-grid-item ><div class="grid-item"><img src="../../images/menu5.png" alt=""><p>文字</p></div></van-grid-item>
+            <van-grid-item ><div class="grid-item"><img src="../../images/menu6.png" alt=""><p>文字</p></div></van-grid-item>
+        </van-grid>
     </div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            swipeItems: [],
+        }
+    },
+    created() {
+        this.getSwipe();
+    },
+    methods: {
+        getSwipe() {
+            this.swipeItems.push('../../images/timg2.jpg')
+        }
+    }
+}
 </script>
 
 <style lang="less" scoped>
@@ -19,6 +39,27 @@
     height: 200px;
     .van-swipe {
         height: 100%;
+        .van-swipe-item {
+            &:nth-child(1) {
+                 background-color: red;
+             }
+        }
+    }
+    .grid-container {
+        .grid-item {
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            img {
+                width: 50%;
+                margin: 0 50%;
+                transform: translateX(-50%);
+            }
+            p {
+                text-align: center;
+            }
+        }
+
     }
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import app from './APP.vue'
 import { Header } from 'mint-ui'
-import { Swipe, SwipeItem } from 'vant'
+import {
+    Swipe,
+    SwipeItem,
+    Grid,
+    GridItem
+} from 'vant'
+
+/* Vue相关 */
+import VueResource from 'vue-resource'
 
 /* minui */
 import 'mint-ui/lib/header/style.css'
@@ -10,6 +18,8 @@ import 'mint-ui/lib/header/style.css'
 /* vant */
 import 'vant/lib/swipe/index.css'
 import 'vant/lib/swipe-item/index.css'
+import 'vant/lib/grid/index.css'
+import 'vant/lib/grid-item/index.css'
 
 /* 外部组件 */
 import './lib/mui/css/mui.min.css'
@@ -18,7 +28,11 @@ import './lib/mui/css/icons-extra.css'
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Grid.name, Grid)
+Vue.component(GridItem.name, GridItem)
+
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 
 import router from './router'
